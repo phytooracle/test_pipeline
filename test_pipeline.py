eb3f0704848596314300d04bb9119cc89cd60027
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-12-10 08:43:18 -0700

    changes to tar

--- test_pipeline.py
+++ test_pipeline.py
@@ -125,11 +125,11 @@ def tar_outputs(scan_date, dictionary):
         if not os.path.isdir(os.path.join(cwd, scan_date, outdir)):
             os.makedirs(os.path.join(cwd, scan_date, outdir))
 
-            file_path = os.path.join(cwd, scan_date, outdir, f'{scan_date}_{v}_plants.tar') 
-            print(f'Creating {file_path}.')
-            if not os.path.isfile(file_path):
-                with tarfile.open(file_path, 'w') as tar:
-                    tar.add(v, recursive=True)
+        file_path = os.path.join(cwd, scan_date, outdir, f'{scan_date}_{v}_plants.tar') 
+        print(f'Creating {file_path}.')
+        if not os.path.isfile(file_path):
+            with tarfile.open(file_path, 'w') as tar:
+                tar.add(v, recursive=True)
 
 
 
