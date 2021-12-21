4d2d8cfa9a114bd8221315a181e9a5dff00f8f33
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-12-21 13:23:22 -0700

    change write mode

--- distributed_pipeline_wrapper.py
+++ distributed_pipeline_wrapper.py
@@ -280,7 +280,7 @@ def launch_workers(account, partition, job_name, nodes, number_tasks, number_tas
         fh.writelines(f"#SBATCH --time={time}\n")
         fh.writelines("export CCTOOLS_HOME=${HOME}/cctools-7.1.12-x86_64-centos7")
         fh.writelines("export PATH=${CCTOOLS_HOME}/bin:$PATH")
-        fh.writelines(f"${HOME}/cctools-7.1.12-x86_64-centos7/bin/work_queue_factory -T slurm -B '--account={account} --partition={partition} --job-name={job_name} --time={time} --mem-per-cpu={mem_per_cpu}GB' -M {manager_name} -w {min_worker} -W {max_worker} --workers-per-cycle 0 --cores={cores} -t {worker_timeout}")
+        fh.writelines(f"work_queue_factory -T slurm -B '--account={account} --partition={partition} --job-name={job_name} --time={time} --mem-per-cpu={mem_per_cpu}GB' -M {manager_name} -w {min_worker} -W {max_worker} --workers-per-cycle 0 --cores={cores} -t {worker_timeout}")
 
     os.system(f"sbatch {outfile}")
 
