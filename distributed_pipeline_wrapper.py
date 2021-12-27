7d1c31b9d99a7d0a08d585e1b74df83e0f9a2ad3
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-12-26 20:28:26 -0700

    uploading

--- distributed_pipeline_wrapper.py
+++ distributed_pipeline_wrapper.py
@@ -613,7 +613,7 @@ def upload_outputs(date, dictionary):
 
     if args.hpc: 
         print('>>>>>>Using data transfer node.')
-        sp.call(f"ssh filexfer 'icd {root}' '&& iput -rfKPVT {date}' '&& exit'", shell=True)
+        sp.call(f"ssh filexfer 'cd {cwd}' '&& icd {root}' '&& iput -rfKPVT {date}' '&& exit'", shell=True)
 
     else:
         
