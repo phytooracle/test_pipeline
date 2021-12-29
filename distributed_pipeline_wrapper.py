afb5028d6327acb29e00998506191d0786b9e0db
emmanuelgonz <emmanuelgonzalez@email.arizona.edu>
2021-12-28 19:09:58 -0700

    formatting and refactoring

--- distributed_pipeline_wrapper.py
+++ distributed_pipeline_wrapper.py
@@ -71,7 +71,7 @@ def get_args():
     parser.add_argument('-d',
                         '--date',
                         help='Date to process',
-                        metavar='str',
+                        nargs='+',
                         type=str,
                         required=True)
 
@@ -278,6 +278,22 @@ def download_level_1_data(irods_path):
         sp.call(f"cd {os.path.join(cwd, direc)}")
         sp.call(cmd2, shell=True)
         sp.call(cmd3, shell=True)
+
+
+# --------------------------------------------------
+def get_required_files_3d(dictionary, date):
+    level_1 = dictionary['paths']['cyverse']['input']['basename']
+    if dictionary['tags']['sensor']=='scanner3DTop':
+        cwd = os.getcwd()
+        irods_data_path = os.path.join(level_1, date, 'alignment')
+        if not os.path.isdir('alignment'):
+            download_level_1_data(irods_data_path)
+        if not os.path.isfile('transfromation.json'):
+            get_transformation_file(os.path.join(level_1, date), cwd)
+        if not os.path.isfile('stereoTop_full_season_clustering.csv'):
+            get_season_detections()
+        if not os.path.isfile('gcp_season_10.txt'):
+            get_gcp_file()
 
 
 # --------------------------------------------------
@@ -731,70 +747,56 @@ def main():
 
     args = get_args()
     cctools_path = download_cctools()
-    clean_directory()
-    
-    with open(args.yaml, 'r') as stream:
-        try:
-            global dictionary
-            dictionary = yaml.safe_load(stream)
-            build_containers(dictionary)
+    for date in args.date:
 
-        except yaml.YAMLError as exc:
-            print(exc)
+        clean_directory()
+        kill_workers(dictionary['workload_manager']['job_name'])
         
-        kill_workers(dictionary['workload_manager']['job_name'])
-        if args.hpc:
-            launch_workers(account=dictionary['workload_manager']['account'], 
-                    partition=dictionary['workload_manager']['partition'], 
-                    job_name=dictionary['workload_manager']['job_name'], 
-                    nodes=dictionary['workload_manager']['nodes'], 
-                    number_tasks=dictionary['workload_manager']['number_tasks'], 
-                    number_tasks_per_node=dictionary['workload_manager']['number_tasks_per_node'], 
-                    time=dictionary['workload_manager']['time_minutes'], 
+        with open(args.yaml, 'r') as stream:
+            try:
+                global dictionary
+                dictionary = yaml.safe_load(stream)
+                build_containers(dictionary)
 
-                    mem_per_cpu=dictionary['workload_manager']['mem_per_cpu'], 
-                    manager_name=dictionary['workload_manager']['manager_name'], 
-                    min_worker=dictionary['workload_manager']['min_worker'], 
-                    max_worker=dictionary['workload_manager']['max_worker'], 
-                    cores=dictionary['workload_manager']['cores_per_worker'], 
-                    worker_timeout=dictionary['workload_manager']['worker_timeout_seconds'])
+            except yaml.YAMLError as exc:
+                print(exc)
 
-        # cyverse_path = os.path.join(dictionary['paths']['cyverse']['input']['basename'], 
-        #                                 args.date,
-        #                                 dictionary['paths']['cyverse']['input']['subdir'], 
-        #                                 ''.join([str(args.date), str(dictionary['paths']['cyverse']['input']['suffix'])]))
-                                        
-        # dir_name = download_raw_data(cyverse_path)
-        global seg_model_name, det_model_name
-        seg_model_name, det_model_name = get_model_files(args.seg_model, args.det_model)
+            get_required_files_3d(dictionary=dictionary, date=date)
+            
+            if args.hpc:
+                launch_workers(account=dictionary['workload_manager']['account'], 
+                        partition=dictionary['workload_manager']['partition'], 
+                        job_name=dictionary['workload_manager']['job_name'], 
+                        nodes=dictionary['workload_manager']['nodes'], 
+                        number_tasks=dictionary['workload_manager']['number_tasks'], 
+                        number_tasks_per_node=dictionary['workload_manager']['number_tasks_per_node'], 
+                        time=dictionary['workload_manager']['time_minutes'], 
 
-        for k, v in dictionary['modules'].items():
-            level_1 = dictionary['paths']['cyverse']['input']['basename']
-            dir_name = os.path.join(*v['input_dir'])
-            if dictionary['tags']['sensor']=='scanner3DTop':
-                cwd = os.getcwd()
-                irods_data_path = os.path.join(level_1, args.date, 'alignment')
-                if not os.path.isdir('alignment'):
-                    download_level_1_data(irods_data_path)
-                if not os.path.isfile('transfromation.json'):
-                    get_transformation_file(os.path.join(level_1, args.date), cwd)
-                if not os.path.isfile('stereoTop_full_season_clustering.csv'):
-                    get_season_detections()
-                if not os.path.isfile('gcp_season_10.txt'):
-                    get_gcp_file()
+                        mem_per_cpu=dictionary['workload_manager']['mem_per_cpu'], 
+                        manager_name=dictionary['workload_manager']['manager_name'], 
+                        min_worker=dictionary['workload_manager']['min_worker'], 
+                        max_worker=dictionary['workload_manager']['max_worker'], 
+                        cores=dictionary['workload_manager']['cores_per_worker'], 
+                        worker_timeout=dictionary['workload_manager']['worker_timeout_seconds'])
 
-            files_list = get_file_list(dir_name, level=v['file_level'], match_string=v['input_file'])
-            write_file_list(files_list)
-            json_out_path = generate_makeflow_json(level=v['file_level'], files_list=files_list, command=v['command'], container=v['container']['simg_name'], inputs=v['inputs'], outputs=v['outputs'], date=args.date, sensor=dictionary['tags']['sensor'], json_out_path=f'wf_file_{k}.json')
-            run_jx2json(json_out_path, cctools_path, batch_type=v['distribution_level'], manager_name=dictionary['workload_manager']['manager_name'], retries=dictionary['workload_manager']['retries'], port=dictionary['workload_manager']['port'], out_log=True)
-            clean_directory()
-    
+            global seg_model_name, det_model_name
+            seg_model_name, det_model_name = get_model_files(args.seg_model, args.det_model)
 
-        tar_outputs(args.date, dictionary)
-        create_pipeline_logs(args.date)
-        upload_outputs(args.date, dictionary)
-        clean_inputs(args.date)
-        kill_workers(dictionary['workload_manager']['job_name'])
+            for k, v in dictionary['modules'].items():
+                
+                dir_name = os.path.join(*v['input_dir'])
+                files_list = get_file_list(dir_name, level=v['file_level'], match_string=v['input_file'])
+                write_file_list(files_list)
+                json_out_path = generate_makeflow_json(level=v['file_level'], files_list=files_list, command=v['command'], container=v['container']['simg_name'], inputs=v['inputs'], outputs=v['outputs'], date=args.date, sensor=dictionary['tags']['sensor'], json_out_path=f'wf_file_{k}.json')
+                run_jx2json(json_out_path, cctools_path, batch_type=v['distribution_level'], manager_name=dictionary['workload_manager']['manager_name'], retries=dictionary['workload_manager']['retries'], port=dictionary['workload_manager']['port'], out_log=True)
+                clean_directory()
+        
+            kill_workers(dictionary['workload_manager']['job_name'])
+            tar_outputs(date, dictionary)
+            create_pipeline_logs(date)
+            upload_outputs(date, dictionary)
+            clean_inputs(date)
+        
 
 
 # --------------------------------------------------
